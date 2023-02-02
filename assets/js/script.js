b5242cb6533e6f30fa5f09f5b4cd3a807ae9de2b
Patrick Hackman <pajohack@gmail.com>
2023-02-02 21:05:25 +0000

    Add refresh icon to header and create function to refresh the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,10 @@ const scissorsDiv = document.getElementById("scissors");
 const lizardDiv = document.getElementById("lizard");
 const spockDiv = document.getElementById("spock");
 
+function refreshPage(){
+    location.reload();
+}
+
 /**
  * Generates a random computer selection from the array of choices.
  * 
@@ -169,14 +173,16 @@ function mainGame() {
 
 mainGame();
 
+// modal script taken from W3School example https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
